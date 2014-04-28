f2440ec6f4f0b8c889bb13986cda705bc0078ad6
chenhaigang888 <chenhaigang888@163.com>
2014-04-28 15:56:26 +0800

    提交testActivity。java

---
+++ wifiPlayer_android_3.0/src/com/wifiplayer/activitys/TestActivity.java
@@ -0,0 +1,16 @@
+package com.wifiplayer.activitys;
+
+import com.wifiplayer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.view_share_view);
+	}
+}
